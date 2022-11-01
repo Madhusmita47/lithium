9bc6655e321f04ade8918c8cc93db1dfc06c496d
Madhusmita47 <madhusmita238091@gmail.com>
2022-11-01 19:45:49 +0530

    24th Assignment

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment=require('moment')
+const date=moment()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://madhusmita_123:5fiVrKsOKBIGJsKe@cluster0.cpbhduk.mongodb.net/Madhusmita-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -16,7 +18,8 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        const date = moment().format("YYYY-MM-DD HH-mm-ss")
+        console.log(date + ',' + req.ip + ',' + req.originalUrl);
         next();
   }
   );
