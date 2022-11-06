189090fec25b874e63a9ace441c0dbc7a18a0d21
Madhusmita47 <madhusmita238091@gmail.com>
2022-11-07 00:17:13 +0530

    27th Assignment

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,27 +4,32 @@ const jwt = require("jsonwebtoken");
 const authenticate = function (req, res, next) {
 
     let token = req.headers["x-auth-token"]
-
-    //If no token is present in the request header return error
     if (!token) return res.send({ status: false, msg: "token must be present" });
 
-    console.log(token);
+    let decodedToken = jwt.verify(token, "functionup-Lithium");
+    if (!decodedToken) return res.send({ status: false, msg: "token is invalid" });
 
-    // If a token is present then decode the token with verify function
-    // verify takes two inputs:
-    // Input 1 is the token to be decoded
-    // Input 2 is the same secret with which the token was generated
-    // Check the value of the decoded token yourself
-    let decodedToken = jwt.verify(token, "functionup-thorium");
-    if (!decodedToken)
-        return res.send({ status: false, msg: "token is invalid" });
-
+   
     next()
 }
 
 
 const authorise = function (req, res, next) {
-    // comapre the logged in user's id and the id in request
+
+    let token = req.headers["x-auth-token"];
+    let decodedToken = jwt.verify(token, "functionup-Lithium");
+    if (!decodedToken) return res.send({ status: false, msg: "token is invalid" });
+
+    console.log(decodedToken)
+
+    let userId = req.params.userId;
+    
+    if (userId != decodedToken.userId) return res.send("Can't login with is this user. You have to modify the request user details.")
+
     next()
 }
+
+
+
 module.exports.authenticate = authenticate
+module.exports.authorise = authorise
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const userController = require("../controllers/userController")
 const mid = require("../middleware/auth")
 
+
+
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
@@ -11,12 +13,12 @@ router.post("/users", userController.createUser)
 
 router.post("/login", userController.loginUser)
 
-//The userId is sent by front end
-router.get("/users/:userId",mid.authenticate, userController.getUserData)
-// router.post("/users/:userId/posts", userController.postMessage)
+router.get("/users/:userId", mid.authenticate, mid.authorise, userController.getUserData)
 
-router.put("/users/:userId", mid.authenticate, userController.updateUser)
-//router.delete('/users/:userId', userController.deleteUser)
-router.delete("/users/:userId", mid.authenticate, userController.deleteuser)
+router.put("/users/:userId", mid.authenticate, mid.authorise, userController.updateUser)
+
+router.delete("/users/:userId", mid.authenticate, mid.authorise, userController.deleteuser)
+
+
 
 module.exports = router;
