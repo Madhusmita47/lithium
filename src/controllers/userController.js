5994e48cd944b273c65eb3cdfcdfc633e7bba40b
Madhusmita47 <madhusmita238091@gmail.com>
2022-11-08 18:13:27 +0530

    28th Assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,46 +1,109 @@
-const UserModel= require("../models/userModel")
+const userModel = require("../models/userModel");
+const jwt = require('jsonwebtoken')
 
 
+const createUser = async function (abcd, xyz) {
+    try {
 
+        let data = abcd.body;
+        let savedData = await userModel.create(data);
 
-const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
+        xyz.status(201).send({ msg: savedData });
 
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
+    } catch (error) {
+        xyz.status(500).send({ status: false, msg: error.message })
     }
 
+};
+
+const loginUser = async function (req, res) {
+
+    try {
+
+        let userName = req.body.emailId;
+        let password = req.body.password;
+
+        let user = await userModel.findOne({ emailId: userName, password: password });
+        if (!user) return res.status(400).send({ status: false, msg: "username or the password is not corerct" });
+
+        let token = jwt.sign(
+            {
+                userId: user._id.toString(),
+                batch: "Lithium",
+                organisation: "FunctionUp",
+            },
+            "madhu");
+
+        res.setHeader("x-auth-token", token);
+        res.status(200).send({ status: true, data: token });
+
+    } catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
+
+    }
+
+};
+
+const getUserData = async function (req, res) {
+
+    try {
+        let userId = req.params.userId;
+        let userDetails = await userModel.findById(userId);
+        if (!userDetails) return res.status(404).send({ status: false, msg: "No such user exists" });
+
+        res.status(200).send({ status: true, data: userDetails });
+
+    } catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
+
+    }
+
+};
+
+const updateUser = async function (req, res) {
+
+    try {
+        let userId = req.params.userId;
+        let user = await userModel.findById(userId);
+
+        if (!user) return res.status(404).send("No such user exists");
+
+        let userData = req.body;
+        let updatedUser = await userModel.findOneAndUpdate({ _id: userId /*, isDeleted: false*/ }, userData, { new: true });
+        // if (!updatedUser) return res.send({ status: false, msg: "User data not found or already deleted!" })
 
 
+        res.status(200).send({ status: updatedUser, data: updatedUser });
+    } catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
+
+    }
+
+};
 
 
+const deleteuser = async function (req, res) {
 
+    try {
+        let userId = req.params.userId
 
+        let deleteUser = await userModel.findOneAndUpdate({ _id: userId /*, isDeleted: false*/ }, { isDeleted: true })
+        // if (!deleteUser) return res.send({ status: false, msg: "User data not found or already deleted!" })
 
+        res.status(200).send({ status: true, msg: " Successfully Deleted" })
 
+    } catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
 
+    }
 
-
-
-
-
-
-
-
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
+
+
+module.exports.createUser = createUser;
+module.exports.getUserData = getUserData;
+module.exports.updateUser = updateUser;
+module.exports.loginUser = loginUser;
+module.exports.deleteuser = deleteuser
--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,26 +1,51 @@
+const jwt = require("jsonwebtoken");
 
-const mid1= function ( req, res, next) {
-    req.falana= "hi there. i am adding something new to the req object"
-    console.log("Hi I am a middleware named Mid1")
-    next()
+
+const authenticate = function (req, res, next) {
+
+    try {
+        let token = req.headers["x-auth-token"]
+        if (!token) return res.status(400).send({ status: false, msg: "token must be present" });
+
+        let decodedToken = jwt.verify(token, "functionup-Lithium");
+        if (!decodedToken) return res.status(401).send({ status: false, msg: "token is invalid" });
+
+        req.token = decodedToken
+
+        next()
+        
+    } catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
+
+    }
+
 }
 
-const mid2= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid2")
-    next()
+
+const authorise = function (req, res, next) {
+
+    try {
+        // let token = req.headers["x-auth-token"];
+
+        // let decodedToken = jwt.verify(token, "madhu");
+        // if (!decodedToken) return res.status(400).send({ status: false, msg: "token is invalid" });
+
+        const actUserId = req.token.userId
+        const reqUserId = req.params.userId
+        // console.log(actUserId)
+
+        if (actUserId !== reqUserId) return res.status(403).send({ status: false, msg: "Bad Login" })
+
+        next()
+
+    } catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
+
+    }
+
 }
 
-const mid3= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid3")
-    next()
-}
 
-const mid4= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid4")
-    next()
-}
 
-module.exports.mid1= mid1
-module.exports.mid2= mid2
-module.exports.mid3= mid3
-module.exports.mid4= mid4
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
